b853c02c2fe98ba4c01875b09098551e924d8fc8
GizemUlusakarya <gizem.techpro@gmail.com>
2024-09-28 18:55:02 +0100

    v3

--- src/main/java/first_collaboration/Gizems_class.java
+++ src/main/java/first_collaboration/Gizems_class.java
@@ -2,6 +2,6 @@ package first_collaboration;
 
 public class Gizems_class {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello,");
     }
 }
