3d9f5ac557f116f8657173339f50ee62f8781003
Kemal Ergorun <kemalergorun@gmail.com>
2024-09-30 14:10:13 +0300

    ver 30/09 son

---
+++ src/main/java/first_collaboration/Kemale.java
@@ -0,0 +1,12 @@
+package first_collaboration;
+
+public class Kemale {
+    public static void main(String[] args) {
+        System.out.println("hi");
+        System.out.println("easy");
+
+
+
+
+    }
+}
